4c0c3996b342a93f58ccace473e5d8a0edbb0baa
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-12 06:36:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,4 +2,6 @@
 first_number = 1
 second_number = 2
 sum = first_number + second_number
-difference = 
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
